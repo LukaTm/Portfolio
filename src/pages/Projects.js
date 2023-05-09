4b8f7eb0c712c7b72a67fda73eec69046748da4e
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-05-10 01:29:21 +0300

    Save

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,26 +3,22 @@ import styles from "./Projecets.module.css";
 function Projects() {
     return (
         <div>
-            <h1>Projects</h1>
+            <div className={styles.title}>
+                <h1>Projects</h1>
+            </div>
+
             <div className={styles.container}>
                 <section className={styles.section}>
                     <div className={styles.rock}>
-                        <div>
-                            <div className={styles.button}>Button</div>
-                        </div>
+                        <div className={styles.button}>Button</div>
                     </div>
                     <div className={styles.calc}>
-                        <div>
-                            <div className={styles.button}>Button</div>
-                        </div>
-                        {/* <div className={styles.button}>Button</div> */}
+                        <div className={styles.button}>Button</div>
                     </div>
                     <div></div>
                     <div></div>
                     <div className={styles.todo}>
-                        <div>
-                            <div className={styles.button}>Button</div>
-                        </div>
+                        <div className={styles.button}>Button</div>
                     </div>
                 </section>
             </div>

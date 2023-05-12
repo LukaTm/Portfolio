d5655ffea9677dd1525387cffdb8d3deaf5bf4ed
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-05-13 01:35:26 +0300

    Skills added

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,18 +12,58 @@ function About() {
                 ducimus laborum pariatur dicta fuga placeat facere dolore.
             </div>
             <div>My Skills</div>
-            <section>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </section>
+            <div className={styles.outside_container}>
+                <section>
+                    <div class={styles.skill_container}>
+                        <div class={styles.html}></div>
+                        <div>HTML5</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.javascript}></div>
+                        <div>JavaScript</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.css}></div>
+                        <div>CSS3</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.express}></div>
+                        <div>Expressjs</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.node}></div>
+                        <div>Node</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.react}></div>
+                        <div>React</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.redux}></div>
+                        <div>Redux</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.git}></div>
+                        <div>Git</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.webpack}></div>
+                        <div>Wepback</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.vsscode}></div>
+                        <div>vssode</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.restful}></div>
+                        <div>RESTful API</div>
+                    </div>
+                    <div class={styles.skill_container}>
+                        <div className={styles.mongoose}></div>
+                        <div>mongoose</div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }

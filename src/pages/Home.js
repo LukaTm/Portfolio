d1a79e006914a7965a56f0f47e045adede06ffce
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-06-11 22:59:07 +0300

    save

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -75,11 +75,11 @@ function Home() {
         console.log(containerCenterX);
 
         // X and Y CLIENT coordinates
-        const offsetX = (event.clientX - containerCenterX) * 0.07; // Adjust the sensitivity by changing the multiplication value
-        const offsetY = (event.clientY - containerCenterY) * 0.07;
+        const offsetX = (event.clientX - containerCenterX) * 0.04; // Adjust the sensitivity by changing the multiplication value
+        const offsetY = (event.clientY - containerCenterY) * 0.05;
         // 4-8 PARTICLES
-        const secondOffsetX = (event.clientX - containerCenterX) * 0.05; // Adjust the sensitivity by changing the multiplication value
-        const secondOffsetY = (event.clientY - containerCenterY) * 0.05;
+        const secondOffsetX = (event.clientX - containerCenterX) * 0.02; // Adjust the sensitivity by changing the multiplication value
+        const secondOffsetY = (event.clientY - containerCenterY) * 0.02;
 
         dispatch({
             type: "MOVE_PARTICLES_ONE_TO_FOUR",
@@ -122,68 +122,70 @@ function Home() {
                 kjvp91vjk1 pjv1vj1v1v
             </div>
             <h2>FIND ME ON</h2>
-            <div
-                className="particle-wrapper h-80 w-96"
-                ref={blocksContainer}
-                onMouseMove={handleMouseMove}
-            >
-                {/* div.particle$*8 */}
+            <div className="particle-outside-wrapper -z-10">
                 <div
-                    className="particle1 "
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle1.x}px, ${particle1.y}px, 0) rotate(51deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle2"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle2.x}px, ${particle2.y}px, 0) rotate(20deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle3"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle3.x}px, ${particle3.y}px, 0) rotate(-17deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle4"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle4.x}px, ${particle4.y}px, 0) rotate(26deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle5"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle5.x}px, ${particle5.y}px, 0) rotate(-11deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle6"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle6.x}px, ${particle6.y}px, 0) rotate(14deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle7"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle7.x}px, ${particle7.y}px, 0) rotate(-14deg)`,
-                    }}
-                ></div>
-                <div
-                    className="particle8"
-                    style={{
-                        // (tx, ty, tz)
-                        transform: `translate3d(${particle8.x}px, ${particle8.y}px, 0) rotate(17deg)`,
-                    }}
-                ></div>
+                    className="particle-wrapper h-80 w-96 "
+                    ref={blocksContainer}
+                    onMouseMove={handleMouseMove}
+                >
+                    {/* div.particle$*8 */}
+                    <div
+                        className="particle1 "
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle1.x}px, ${particle1.y}px, 0) rotate(51deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle2"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle2.x}px, ${particle2.y}px, 0) rotate(20deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle3"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle3.x}px, ${particle3.y}px, 0) rotate(-17deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle4"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle4.x}px, ${particle4.y}px, 0) rotate(26deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle5"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle5.x}px, ${particle5.y}px, 0) rotate(-11deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle6"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle6.x}px, ${particle6.y}px, 0) rotate(14deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle7"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle7.x}px, ${particle7.y}px, 0) rotate(-14deg)`,
+                        }}
+                    ></div>
+                    <div
+                        className="particle8"
+                        style={{
+                            // (tx, ty, tz)
+                            transform: `translate3d(${particle8.x}px, ${particle8.y}px, 0) rotate(17deg)`,
+                        }}
+                    ></div>
+                </div>
             </div>
         </div>
     );
